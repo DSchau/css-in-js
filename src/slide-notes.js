ee26e75e7248ceb8d59a0a46d90c24521032e9f5
Dustin Schau <dustinschau@gmail.com>
2017-10-28 01:38:00 -0500

    remove unused file

--- src/slide-notes.js
+++
@@ -1,7 +0,0 @@
-const context = require.context('./slides', true, /index\.js$/);
-
-export default context.keys().reduce((notes, key) => {
-  const file = context(key);
-  notes[key] = file;
-  return notes;
-}, {});
